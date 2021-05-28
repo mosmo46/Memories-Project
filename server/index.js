ccef7045d9bbcd9f6fa44f3b3f87980db2eb3282
mosmo46 <73312834+mosmo46@users.noreply.github.com>
2021-05-29 01:24:07 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -10,14 +10,23 @@ dotenv.config()
 app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 app.use('/posts',postRoutes)
 app.get('/',(req,res)=>{res.send('Welcome to my API')})
 
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>{console.log(`Server Running on PORT ${PORT}`)}))
 .catch((err)=>console.log(err.message))
 
 mongoose.set('useFindAndModify', false);
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
